e0a559357eca6f51d772f37b9adf8b7560fea007
Eric Internicola <intere@gmail.com>
2013-07-26 13:28:31 -0600

    Refactoring and fixes that remove the state of the iTunesSearchService and test more than just the happy path.

--- app/scripts/services/iTunesSearchService.js
+++ app/scripts/services/iTunesSearchService.js
@@ -8,25 +8,18 @@ angular.module('itunesServiceApp')
   .service('iTunesSearchService', function($http, $log) {
 
   	//
-  	// Refactor: moving the base URLs into the service so they can be exposed externally
-  	//
-
-	// var SEARCH_URL = 'https://itunes.apple.com/search';
-	// var LOOKUP_URL = "https://itunes.apple.com/lookup?id=";
-
-
 	// Create the Service
+	//
 	var service = {
 
 		SEARCH_URL: 'https://itunes.apple.com/search',
 		LOOKUP_URL: "https://itunes.apple.com/lookup?id=",
 
-		searchText: undefined,
-		searchType: undefined,
-		resultType: undefined,
-		limit: undefined,
-		results: [],
-
+		//
+		// Refactor:  Let's now use the state of the service object, instead lets define a 
+		// contract where we have an object that contains the state for the search.  Let's
+		// create a helper method that will create the object for you too!
+		//
 		get: function() {
 			var url = SEARCH_URL
 				+ '?term=' + this.searchText
@@ -57,25 +50,41 @@ angular.module('itunesServiceApp')
 		},
 
 		//
+		// Builds the search object for you.  The result of this can be passed to the get function.
+		//
+		buildSearchObject: function(searchText, searchType, resultType, limit) {
+			var searchObject = {
+				'searchText': searchText,
+				'searchType': searchType,
+				'resultType': resultType,
+				'limit': limit
+			};
+
+			if(!searchText) {
+				throw "You must provide search text if you'd like to do a search";
+			}
+
+			return searchObject;
+		},
+
+		//
 		// Adding a method that builds the URL for you, now we can test it in isolation!
 		//
-		buildSearchUrl: function() {
+		buildSearchUrl: function(searchCriteria) {
 
-			//
-			// WHOA - Unit tests showed us that we have issues:
-			//   TypeError: Cannot read property 'SEARCH_URL' of undefined
-	    	// 		at null.<anonymous> (/Users/einternicola/code/itunes-service/test/spec/services/iTunesSearchService.js:21:42)
-			//
+			var url = this.SEARCH_URL + '?term=' + searchCriteria.searchText;
+			
+			if(searchCriteria.searchType) {
+				url += '&attribute=' + searchCriteria.searchType;
+			}
+			if(searchCriteria.resultType) {
+				url += '&entity=' + searchCriteria.resultType;
+			}
+			if(searchCriteria.limit) {
+				url += "&limit=" + searchCriteria.limit;
+			}
 
-			//
-			// 
-			//
-			var url = this.SEARCH_URL
-				+ '?term=' + this.searchText
-				+ (this.searchType ? '&attribute=' + this.searchType : "")
-				+ (this.resultType ? '&entity=' + this.resultType : "")
-				+ (this.limit ? "&limit=" + this.limit: "")
-				+ '&callback=JSON_CALLBACK';
+			url += '&callback=JSON_CALLBACK';
 
 			return url;
 		}
--- test/spec/services/iTunesSearchService.js
+++ test/spec/services/iTunesSearchService.js
@@ -2,6 +2,11 @@
 
 describe('Service: iTunesSearchService', function () {
 
+  var SEARCH_TEXT = "Rob Zombie";
+  var SEARCH_TYPE = "Artist";
+  var RESULT_TYPE = "Album";
+  var LIMIT = 100;
+
   // load the service's module
   beforeEach(module('itunesServiceApp'));
 
@@ -11,25 +16,91 @@ describe('Service: iTunesSearchService', function () {
     iTunesSearchService = _iTunesSearchService_;
   }));
 
+
   it('should do something', function () {
     expect(!!iTunesSearchService).toBe(true);
   });
 
   //
-  // Bug 1:  We forgot to "inject" the service into the test - now fixed
+  // Verifies that we correctly build a Search Object:
+  //
+  it('should build a search criteria object for us', inject(function($log, iTunesSearchService) {
+
+    // Test 1: throwing an exception:
+    failBuildObject();
+    failBuildObject(undefined, SEARCH_TYPE);
+    failBuildObject(undefined, undefined, RESULT_TYPE);
+    failBuildObject(undefined, undefined, undefined, LIMIT);
+    failBuildObject(undefined, SEARCH_TYPE, RESULT_TYPE, LIMIT);
+
+    // Test 2: test valid object building cases:
+    testBuildObject(SEARCH_TEXT);
+    testBuildObject(SEARCH_TEXT, SEARCH_TYPE);
+    testBuildObject(SEARCH_TEXT, undefined, RESULT_TYPE);
+    testBuildObject(SEARCH_TEXT, undefined, undefined, LIMIT);
+    testBuildObject(SEARCH_TEXT, undefined, RESULT_TYPE, LIMIT);
+    testBuildObject(SEARCH_TEXT, SEARCH_TYPE, RESULT_TYPE, LIMIT);
+
+    //
+    // Test Helper Functions:
+    //
+    function testBuildObject(sText, sType, rType, lim) {
+      var result = iTunesSearchService.buildSearchObject(sText, sType, rType, lim);
+      expect(result).toNotBe(undefined);
+      expect(result.searchText).toBe(sText);
+      expect(result.searchType).toBe(sType);
+      expect(result.resultType).toBe(rType);
+      expect(result.limit).toBe(lim);
+    }
+
+    function failBuildObject(sText, sType, rType, lim) {
+      try {
+        var result = iTunesSearchService.buildSearchObject(sText, sType, rType, lim); 
+        // if we got here, we didn't throw an exception
+        expect(false).toBe(true);
+      } catch(reason) {
+        // Expected behavior        
+      }
+    }
+  }));
+
+  //
+  // Verifies that we correctly build the URL.
   //
   it('should build the search URL correctly', inject(function(iTunesSearchService){
 
-    var searchText = 'Rob Zombie';
-    var expectedUrl = iTunesSearchService.SEARCH_URL + '?term=' + searchText + "&callback=JSON_CALLBACK";
-
-    iTunesSearchService.searchText = searchText;
-    expect(iTunesSearchService.buildSearchUrl()).toBe(expectedUrl);
+    testBuildUrlFor(SEARCH_TEXT);
+    testBuildUrlFor(SEARCH_TEXT, SEARCH_TYPE);
+    testBuildUrlFor(SEARCH_TEXT, SEARCH_TYPE, RESULT_TYPE);
+    testBuildUrlFor(SEARCH_TEXT, SEARCH_TYPE, RESULT_TYPE, LIMIT);
+    testBuildUrlFor(SEARCH_TEXT, SEARCH_TYPE, undefined, LIMIT);
+    testBuildUrlFor(SEARCH_TEXT, undefined, RESULT_TYPE, LIMIT);
+    testBuildUrlFor(SEARCH_TEXT, undefined, undefined, LIMIT);
 
     //
-    // Now - go run the tests and see what happens:
-    // $ grunt test
+    // Test Helper function
     //
+    function testBuildUrlFor(sText, sType, rType, lim) {
+
+      // First - build the expected URL:
+      var expectedUrl = iTunesSearchService.SEARCH_URL + "?term=" + sText;
+      if(sType) {
+        expectedUrl += "&attribute=" + sType;
+      }
+      if(rType) {
+        expectedUrl += "&entity=" + rType;
+      }
+      if(lim) {
+        expectedUrl += "&limit=" + lim;
+      }
+      expectedUrl += "&callback=JSON_CALLBACK";
+
+      // Now get the generated one:
+      var actualUrl = iTunesSearchService.buildSearchUrl(iTunesSearchService.buildSearchObject(sText, sType, rType, lim));
+
+      // Now perform the verification
+      expect(actualUrl).toBe(expectedUrl);      
+    }
 
   }));
 
